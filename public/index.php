d28735fb6fb916ea04de5813bbce5d2ab577b907
daniel <daniel.pozzan@hotmail.com>
2026-01-19 11:01:19 +0100

    Refactor router to class

--- public/index.php
+++ public/index.php
@@ -1,7 +1,16 @@
 <?php 
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+require basePath('Router.php');
 
-require basePath('router.php');
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+
+
 

2a68780aa78bcb5c12b60dcd4501408ac262236e
daniel <daniel.pozzan@hotmail.com>
2026-02-01 21:48:22 +0100

    Replace query params with route params

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -39,14 +39,19 @@ class ListingController {
      *
      * @return void
      */
-    public function show() {
-        $id = $_GET['id'] ?? '';
-
+    public function show($params) {
+        $id = $params['id'] ?? '';
+        
         $params = [
             'id' => $id
         ];
 
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if(!$listing){
+            ErrorController::notFound('Listing not found');
+            return;
+        }
 
         loadView('listings/show', [
             'listing' => $listing

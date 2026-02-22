226d0cacc679545ff2ff141009d939e60e731594
daniel <daniel.pozzan@hotmail.com>
2026-02-22 18:32:51 +0100

    Add registration

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -71,8 +71,40 @@ class UserController {
                 ]
             ]);
             exit;
-        } else {
-            inspectAndDie('Store');
+        } 
+
+        // Check if email exists
+        $params = [
+            'email' => $email
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+
+        if($user){
+            $errors['email'] = 'That email already exists';
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state
+                ]
+            ]);
+            exit;
         }
+
+        // Create user account
+        $params = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+        $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        redirect('/');
     }
 }
